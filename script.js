fe4135469a5e9b4061fb3af81d75d6a75b36b4c1
arthur <arthur.dartois@gmail.com>
2018-09-27 12:17:30 +0200

    ab

---
+++ script.js
@@ -0,0 +1,13 @@
+
+setTimeout(function()
+
+
+ { if (confirm("T'aimes les Monaco de Belin?")) {
+   window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+} else {
+   txt = "You pressed Cancel!";
+  }
+
+}, 10000)
+
+;
